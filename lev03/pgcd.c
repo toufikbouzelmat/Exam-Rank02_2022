20f5548e1679fbcc58f94c0a467c4a9f9ba1c02d
Taoufik <toufikbouzelmat@outlook.fr>
2022-07-04 20:23:20 +0100

    modif in ex hidenp

--- lev03/pgcd.c
+++ lev03/pgcd.c
@@ -6,7 +6,7 @@
 /*   By: tbouzalm <tbouzalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 01:09:45 by tbouzalm          #+#    #+#             */
-/*   Updated: 2022/07/02 15:37:20 by tbouzalm         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:29:47 by tbouzalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// int	ft_pcgd(unsigned int nb1, unsigned int nb2)
-// {
-// 	int	n;
-
-// 	n = nb2;
-// 	while (n >= 0)
-// 	{
-// 		if (nb1 % n == 0 && nb2 % n == 0)
-// 			return(n);
-// 		n--;
-// 	}
-// 	return (0);
-// }
-
-void	pcgd(int a, int b)
+int	ft_pcgd(unsigned int nb1, unsigned int nb2)
 {
-	int i;
-	int gcd;
+	int	n;
 
-	i = 1;
-	gcd = 0;
-	while (i < a && i < b)
+	n = nb2;
+	while (n > 0)
 	{
-		if (a % i == 0 && b % i == 0)
-			gcd = i;
-		i++;
+		if (nb1 % n == 0 && nb2 % n == 0)
+			return(n);
+		n--;
 	}
-	printf("%d", gcd);
+	return (0);
 }
+
 
 int	main(int ac, char **av)
 {
-	pcgd(atoi(av[1]),atoi(av[2]));
+	printf("%d",ft_pcgd(atoi(av[1]),atoi(av[2])));
 }
